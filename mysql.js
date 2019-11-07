56e3bba180f057f8e1926a01f313ede0d48dc455
Markus Ahlstrand <markus@ahlstrand.es>
2019-11-07 16:04:19 +0100

    Add dialect when creating client

--- mysql.js
+++ mysql.js
@@ -1,11 +1,12 @@
 const MySqlClient = require("knex/lib/dialects/mysql");
 const dataApi = require("./data-api");
+const constants = require("./constants");
 
 // Call mysql client to setup knex, this set as this function
 function MysqlClientRDSDataAPI(config) {
   MySqlClient.call(this, config);
 }
 
-dataApi(MysqlClientRDSDataAPI, MySqlClient);
+dataApi(MysqlClientRDSDataAPI, MySqlClient, constants.dialects.mysql);
 
 module.exports = MysqlClientRDSDataAPI;
--- postgres.js
+++ postgres.js
@@ -1,11 +1,12 @@
 const PostgresClient = require("knex/lib/dialects/postgres");
 const dataApi = require("./data-api");
+const constants = require("./constants");
 
 // Call mysql client to setup knex, this set as this function
 function PostgresClientRDSDataAPI(config) {
   PostgresClient.call(this, config);
 }
 
-dataApi(PostgresClientRDSDataAPI, PostgresClient);
+dataApi(PostgresClientRDSDataAPI, PostgresClient, constants.dialects.postgres);
 
 module.exports = PostgresClientRDSDataAPI;
