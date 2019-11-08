25407d6d0244732fce3538dff7c9c115603c620f
Markus Ahlstrand <markus@ahlstrand.es>
2019-11-08 10:34:14 +0100

    Added support for more than 10 values...

--- sqlstring.js
+++ sqlstring.js
@@ -3,7 +3,7 @@ const constants = require("./constants");
 
 function format(sql, values, dialect) {
   if (dialect === constants.dialects.postgres) {
-    return sql.replace(/(\$\d)/gi, binding => {
+    return sql.replace(/(\$\d+)/gi, binding => {
       const index = parseInt(binding.slice(1));
       return `'${values[index - 1]}'`;
     });
