fec667e04d2ac9699138b34a1d788a3358664b36
Markus Ahlstrand <markus@ahlstrand.de>
2021-08-27 13:00:43 +0200

    Added a template file to get test setup easier

--- test/integration/migrations-test.js
+++ test/integration/migrations-test.js
@@ -3,6 +3,8 @@ const knexMigrate = require('knex-migrate');
 async function migrateToLatest(knexFilePath) {
   const log = ({ action, migration }) => console.log('Doing ' + action + ' on ' + migration);
 
+  console.log('knexfile: ' + knexFilePath);
+
   await knexMigrate(
     'up',
     {
