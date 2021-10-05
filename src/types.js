5f36eb2ba53c605832fce6fde30d9c86bf5346b4
Christian Castillo <cccrishsocrates@gmail.com>
2021-10-04 22:21:43 -0400

    Fix timestamp precision for select

--- src/types.js
+++ src/types.js
@@ -9,11 +9,11 @@ function applyRecord(columnMetadata, record) {
         case 'timestamptz':
           // Postgres format 2001-01-01 00:00:00 or 2001-01-01 00:00:00.123456
           // eslint-disable-next-line no-case-declarations
-          const [, year, month, day, hour, minute, second] = record[column.name].match(
+          const [, year, month, day, hour, minute, second, millisecond] = record[column.name].match(
             /^(\d{1,4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})(?:.(\d{1,3}))?(?:\d{1,3})?$/,
           );
           parsedColumns[column.name] = new Date(
-            Date.UTC(year, month - 1, day, hour, minute, second),
+            Date.UTC(year, month - 1, day, hour, minute, second, millisecond),
           );
           break;
         case 'json':
