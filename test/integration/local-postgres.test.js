7ffdf702b38d957ccf40461cbd33e180fab996b5
Hasnae <hasnae@viqueen.org>
2022-06-10 09:43:29 +1000

    provide local postgres connection as env var

--- test/integration/local-postgres.test.js
+++ test/integration/local-postgres.test.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { expect } = require('chai');
 
 const commonTests = require('./common-tests');
@@ -5,7 +7,7 @@ const { migrateToLatest } = require('./migrations-test');
 
 const postgres = require('knex')({
   client: 'pg',
-  connection: 'postgres://localhost',
+  connection: process.env.LOCAL_POSTGRES_CONNECTION || 'postgres://localhost',
 });
 
 let counter = 0;
