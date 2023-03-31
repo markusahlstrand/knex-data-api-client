73320f5eca604c9db48946611203ab5f818cbbc5
Daniel Arroyo <daniel.arroyo@gmail.com>
2023-03-31 11:38:21 +0200

    fix: linter errors and adding rollback timeout protection

--- src/data-api-transaction.js
+++ src/data-api-transaction.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable global-require */
 
+const { timeout, KnexTimeoutError } = require('knex/lib/util/timeout');
 const Transaction = require('knex/lib/execution/transaction');
 
 module.exports = class DataAPITransaction extends Transaction {
@@ -39,24 +40,38 @@ module.exports = class DataAPITransaction extends Transaction {
   }
 
   // eslint-disable-next-line class-methods-use-this
-  async rollback(connection) {
+  async rollback(connection, err) {
+    this._completed = true;
     if (connection.isTransaction && connection.rdsTransactionId) {
-      this._completed = true;
-      try {
-        await connection.rollbackTransaction({transactionId: connection.rdsTransactionId }).then(() => {
-          connection.__knexTxId = null;
-          connection.isTransaction = false;
-          connection.rdsTransactionId = null;
-        });
-
-        this._resolver();
-      } catch (err) {
+      return timeout(new Promise((resolve, reject) => {
+        try {
+          connection.rollbackTransaction({transactionId: connection.rdsTransactionId }).then(() => {
+            connection.__knexTxId = null;
+            connection.isTransaction = false;
+            connection.rdsTransactionId = null;
+          });
+  
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      }), 5000)
+      .catch((e) => {
+        if (!(e instanceof KnexTimeoutError)) {
+          return Promise.reject(e);
+        }
+        this._rejecter(e);
+      })
+      .then(() => {
+        if (err === undefined) {
+          if (this.doNotRejectOnRollback) {
+            this._resolver();
+            return;
+          }
+          err = new Error(`Transaction rejected with non-error: ${err}`);
+        }
         this._rejecter(err);
-      }
-      
-      return;
+      })      
     }
-
-    return;
   }
 };
